7d55add410e4a10f0f9c1ddc66e11d8fa970ccc0
Jakalgit <mistetsmit05@gmail.com>
2023-07-29 00:38:04 +0300

    commit

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ const start = async function() {
         const PORT = process.env.PORT || 5000
         const app = await NestFactory.create(AppModule)
 
-        app.enableCors({origin: true, credentials: true})
+        app.enableCors({origin: "http://localhost:3000/", credentials: true})
 
         await app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
     } catch (e) {
